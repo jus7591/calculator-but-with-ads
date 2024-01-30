21e294973ad5f20c2e3b2b9b6f82ca3e0905b2da
Justin Maddox <51386183+jus7591@users.noreply.github.com>
2024-01-30 05:20:12 -0600

    Update Commit 1/30/2024
    -set styling for the modal
    -imported google font Sixtyfour
    -changed variable names to better name that were relevant

--- ad.js
+++ ad.js
@@ -1,11 +1,11 @@
-const microtransaction = document.querySelector('#microtransaction');
+const watchAdButton = document.querySelector('#watch-ad-button');
 const modal = document.querySelector('.modal');
 const link = document.querySelector('#youtube');
 const buttonsContainer = document.querySelector('.buttons');
 const adsRemaining = document.querySelector('#remaining');
 const videos = [
   'https://www.youtube.com/embed/s0brZ8Cinao?si=uCGcEqt4fmfCt7gs',
-  'https://www.youtube.com/embed/zSiNGZQueeM?si=bpvKb3dhx3wNcwR5',
+  'https://www.youtube.com/embed/WPkMUU9tUqk?si=3oSF7TdA__LJQgyX',
   'https://www.youtube.com/embed/wYX_zhlTDr8?si=4Tic_FPJOX1CF6rM',
   'https://www.youtube.com/embed/HqGsT6VM8Vg?si=1M38uxyKH6d_dpwa',
   'https://www.youtube.com/embed/47cAxRX3aDg?si=F7mCVxAS2BYKbkwT',
@@ -15,7 +15,7 @@ const videos = [
 let counter = 0;
 let adCount = 2;
 
-microtransaction.addEventListener('click', () => {
+watchAdButton.addEventListener('click', () => {
   modal.showModal();
   adsLeft();
   playYoutubeLink();
@@ -30,8 +30,8 @@ function playYoutubeLink() {
     divisionBtn.textContent = '/';
     divisionBtn.classList.add('math');
     buttonsContainer.appendChild(divisionBtn);
-    microtransaction.setAttribute('disabled', '');
-    microtransaction.classList.add('disabled');
+    watchAdButton.setAttribute('disabled', '');
+    watchAdButton.classList.add('disabled');
   }
   setTimeout(() => {
     link.src = '';
