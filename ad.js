c262f13a15dd8c2202a276548ffa4347d0e4951a
Justin Maddox <51386183+jus7591@users.noreply.github.com>
2024-01-31 20:38:31 -0600

    Update Commit 1/31/2024
    -updated the commercials that the random youtube video modal would play

--- ad.js
+++ ad.js
@@ -2,15 +2,17 @@ const watchAdButton = document.querySelector('#watch-ad-button');
 const modal = document.querySelector('.modal');
 const link = document.querySelector('#youtube');
 const buttonsContainer = document.querySelector('.buttons');
+const body = document.querySelector('body');
 const adsRemaining = document.querySelector('#remaining');
 const videos = [
   'https://www.youtube.com/embed/s0brZ8Cinao?si=uCGcEqt4fmfCt7gs',
-  'https://www.youtube.com/embed/WPkMUU9tUqk?si=3oSF7TdA__LJQgyX',
   'https://www.youtube.com/embed/wYX_zhlTDr8?si=4Tic_FPJOX1CF6rM',
-  'https://www.youtube.com/embed/HqGsT6VM8Vg?si=1M38uxyKH6d_dpwa',
   'https://www.youtube.com/embed/47cAxRX3aDg?si=F7mCVxAS2BYKbkwT',
   'https://www.youtube.com/embed/CVS1UfCfxlU?si=2xAHnnqcK626zXPY',
   'https://www.youtube.com/embed/N5oYdKJEpNo?si=xnUjRqB2_0y_XGQs',
+  'https://www.youtube.com/embed/7oBZ8sBjdyQ?si=TtpJXvsInZryqZDl',
+  'https://www.youtube.com/embed/9KBXcpJfmj4?si=cP1NWPQ2iLkaOSMw',
+  'https://www.youtube.com/embed/aP2up9N6H-g?si=Uhrj1EoDqxnp7SU0',
 ];
 let counter = 0;
 let adCount = 2;
@@ -22,7 +24,7 @@ watchAdButton.addEventListener('click', () => {
 });
 
 function playYoutubeLink() {
-  const randomNum = Math.floor(Math.random() * 7);
+  const randomNum = Math.floor(Math.random() * 8);
   decideYoutubeVideo(randomNum);
 
   if (counter === 2) {
